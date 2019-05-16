663629ae9bffb620d1934f2a56d504ec3e623f53
Daniel Leong <me@dhleong.net>
2019-05-16 15:07:57 -0400

    Handle devices with cutouts

--- lib/src/main/java/com/interspacehq/quay/KeyboardSizeProvider.kt
+++ lib/src/main/java/com/interspacehq/quay/KeyboardSizeProvider.kt
@@ -3,6 +3,7 @@ package com.interspacehq.quay
 import android.app.Activity
 import android.graphics.Point
 import android.graphics.Rect
+import android.os.Build
 import android.view.Gravity
 import android.view.View
 import android.view.WindowManager
@@ -14,12 +15,16 @@ import io.reactivex.subjects.PublishSubject
  * @author dhleong
  */
 class KeyboardSizeProvider private constructor(
-    private val activity: Activity
+    private val activity: Activity,
+    debug: Boolean
 ) : PopupWindow(activity) {
 
     val heights: Observable<Int> = Observable.defer<Int> {
 
         showAtLocation(activity.findViewById(android.R.id.content), Gravity.NO_GRAVITY, 0, 0)
+
+        contentView.fitsSystemWindows = false
+        contentView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
         Observable.create { emitter ->
             val disposable = heightEvents.distinctUntilChanged()
@@ -41,10 +46,15 @@ class KeyboardSizeProvider private constructor(
         contentView = View(activity)
         softInputMode = WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE or
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE
-        inputMethodMode = PopupWindow.INPUT_METHOD_NEEDED
+        inputMethodMode = INPUT_METHOD_NEEDED
 
         width = 0
         height = WindowManager.LayoutParams.MATCH_PARENT
+
+        if (debug) {
+            width = 100
+            contentView.setBackgroundColor(0xFFff0000.toInt())
+        }
 
         contentView.viewTreeObserver.addOnGlobalLayoutListener(this::onGlobalLayout)
     }
@@ -53,12 +63,32 @@ class KeyboardSizeProvider private constructor(
         activity.windowManager.defaultDisplay.getSize(screenSize)
         contentView.getWindowVisibleDisplayFrame(popupRect)
 
-        val keyboardHeight = screenSize.y - popupRect.bottom
+        // NOTE: the topCutout, if there is any, seems to offset the screen "top" as
+        // used in the computation of getSize() above, so we need to take that into
+        // account when computing the *actual* screen height
+        val effectiveScreenHeight = screenSize.y + topCutoutHeight
+
+        val keyboardHeight = effectiveScreenHeight - popupRect.bottom
         heightEvents.onNext(keyboardHeight)
+    }
+
+    private val topCutoutHeight by lazy(LazyThreadSafetyMode.NONE) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
+            // no cutouts
+            return@lazy 0
+        }
+
+        val decor = activity.window.peekDecorView() ?: return@lazy 0
+        val cutout = decor.rootWindowInsets.displayCutout ?: return@lazy 0
+
+        cutout.boundingRects.maxBy {
+            if (it.top == 0) it.height()
+            else 0
+        }?.height() ?: 0
     }
 
     companion object {
         @Suppress("unused")
-        fun create(activity: Activity) = KeyboardSizeProvider(activity).heights
+        fun create(activity: Activity, debug: Boolean = false) = KeyboardSizeProvider(activity, debug).heights
     }
 }
